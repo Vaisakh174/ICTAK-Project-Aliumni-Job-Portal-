e197ba16212c673451576c871ea2722e096482a9
Vaisakh174 <101011734+Vaisakh174@users.noreply.github.com>
2022-12-24 15:24:51 +0530

    Update approvePostAPI.js

--- router/admin/approvePostAPI.js
+++ router/admin/approvePostAPI.js
@@ -35,22 +35,21 @@ const getCurrentTimeInIST = () => {
     console.log('gmt')
     console.log(gmtTime)
     // Get the time offset for the IST time zone
-    const timeOffset = gmtTime.getTimezoneOffset();
+    // const timeOffset = gmtTime.getTimezoneOffset();
   
     // Calculate the IST time by subtracting the time offset from the GMT time
-    const istTime = new Date(gmtTime - timeOffset * 60 * 1000);
+    const istTime = new Date(gmtTime - (-330) * 60 * 1000);
     console.log('istime')
     console.log(istTime)
     // Format the IST time as a string in the 12-hour clock format
-    const formattedTime =  Date(istTime)
-    //     const formattedTime = istTime.toLocaleString('en-US', {
-    //   year: 'numeric',
-    //   month: 'short',
-    //   day: 'numeric',
-    //   hour: 'numeric',
-    //   minute: 'numeric',
-    //   hour12: true
-    // });
+    const formattedTime = istTime.toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    });
     console.log('formatted time')
     console.log(formattedTime)
     // Return the formatted IST time with the "IST" string appended
